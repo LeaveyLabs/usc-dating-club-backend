d04ad6b4eb83a72ba83633097a931f2cfacf9fd5
Adam Novak <adamnova@usc.edu>
2022-12-22 20:37:32 +0000

    gender options

--- backend/users/models.py
+++ backend/users/models.py
@@ -14,15 +14,17 @@ def profile_picture_filepath(instance, filename) -> str:
 class User(AbstractUser):
     """ User class extension """
 
-    class SexChoices(models.IntegerChoices):
-        """ Enum for questions options. """
-        SLEEP = 0, 'What time do you sleep at night?'
+    class GenderChoices(models.IntegerChoices):
+        """ Enum for gender options. """
+        MEN = 0, 'MEN'
+        WOMEN = 1, 'WOMEN'
+        BOTH = 2, 'BOTH'
 
     email = models.EmailField()
     date_of_birth = models.DateField()
     picture = models.ImageField(upload_to=profile_picture_filepath)
     phone_number = PhoneNumberField()
-    sex = 
+    gender = models.TextField(choices=GenderChoices)
 
 class Question(models.Model):
     """ Compatibility questions for matching users. """
