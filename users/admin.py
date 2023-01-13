fae095a46aadf369fb6e78c3d5bdac80bc0d15d4
kevinsun127 <kevinsun@usc.edu>
2023-01-12 16:18:20 -0800

    register models

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from users.models import EmailAuthentication, PhoneAuthentication, User, Match, Notification
+from users.models import EmailAuthentication, PhoneAuthentication, User, Match, Notification, NumericalResponse, TextResponse, Question
 
 # Register your models here.
 admin.site.register(User)
@@ -8,3 +8,6 @@ admin.site.register(EmailAuthentication)
 admin.site.register(PhoneAuthentication)
 admin.site.register(Match)
 admin.site.register(Notification)
+admin.site.register(NumericalResponse)
+admin.site.register(TextResponse)
+admin.site.register(Question)
