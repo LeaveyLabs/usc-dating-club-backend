05febf34d5108cfef462107c02d9b9389cf6dd65
kevinsun127 <kevinsun@usc.edu>
2023-01-15 10:20:42 -0800

    generalized admin model

--- users/admin.py
+++ users/admin.py
@@ -2,18 +2,6 @@ from django.contrib import admin
 
 from users.models import BannedEmail, EmailAuthentication, PhoneAuthentication, User, Match, Notification, NumericalResponse, TextResponse, Question, WaitingEmail
 
-# Register your models here.
-# admin.site.register(User)
-# admin.site.register(EmailAuthentication)
-# admin.site.register(PhoneAuthentication)
-# admin.site.register(Match)
-# admin.site.register(Notification)
-# admin.site.register(NumericalResponse)
-# admin.site.register(TextResponse)
-# admin.site.register(Question)
-# admin.site.register(WaitingEmail)
-# admin.site.register(BannedEmail)
-
 @admin.register(
     User, 
     EmailAuthentication, 
