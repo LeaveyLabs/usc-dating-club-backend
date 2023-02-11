c2d950065028a078f32bd15f13595b36d6364d68
kevinsun127 <kevinsun@usc.edu>
2023-02-11 12:57:03 -0800

    meters not kilometers

--- users/models.py
+++ users/models.py
@@ -28,7 +28,7 @@ def random_code() -> str:
 
 def haversine(lon1, lat1, lon2, lat2):
     """
-    Calculate the great circle distance in kilometers between two points 
+    Calculate the great circle distance in meters between two points 
     on the earth (specified in decimal degrees)
     """
     if not lon1 or not lat1 or not lon2 or not lat2: 
@@ -43,7 +43,7 @@ def haversine(lon1, lat1, lon2, lat2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
-    return c * r
+    return c * r * 1000
 
 class User(AbstractUser):
     """ User class extension """
@@ -156,6 +156,7 @@ class Match(models.Model):
         """ Notifies users that they've been matched """
         payload1 = self.initial_match_payload(self.user2, self.user1)
         payload2 = self.flip_match_payload(self.user1, payload1)
+        print(payload1)
 
         compatibility = payload1['compatibility']
 
